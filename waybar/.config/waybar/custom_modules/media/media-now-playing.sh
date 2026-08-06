b366295f31322a8e9ee8538c2d674afa833d62b6
Henrik Wöbel <henrik.woebel@gmail.com>
2026-08-06 21:34:25 +0200

    Add music player widget to waybar

--- waybar/.config/waybar/custom_modules/media/media-now-playing.sh
+++ waybar/.config/waybar/custom_modules/media/media-now-playing.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-
-zscroll -l 20 
-    --delay 0.3 
-    --update-check true 
+zscroll -l 20 \
+    --delay 0.3 \
+    --update-check true \
     "playerctl metadata --format '{{title}} - {{artist}}'" 2>/dev/null
-
 wait
