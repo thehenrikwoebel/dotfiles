f46b52cc5db57b7b458362ec95b7aedc8ede89b1
Henrik Wöbel <henrik.woebel@gmail.com>
2026-08-23 20:37:40 +0200

    Add some new waybar, hyprland and rofi styles

--- hyprland/.config/hypr/hyprland.lua
+++ hyprland/.config/hypr/hyprland.lua
@@ -88,8 +88,8 @@ hl.env("HYPRCURSOR_SIZE", "24")
 -- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
 hl.config({
     general = {
-        gaps_in  = 5,
-        gaps_out = 20,
+        gaps_in  = 3,
+        gaps_out = 10,
 
         border_size = 2,
 
